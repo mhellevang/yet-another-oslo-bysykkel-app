5ce6307d771f58810e4c115e2acf570a6c093442
Mathias Hellevang <mathias.hellevang@gmail.com>
2022-01-08 22:07:21 +0100

    La til adresse i eksponert datamodell

--- backend/models/station.js
+++ backend/models/station.js
@@ -1,6 +1,7 @@
 function Station(station, status) {
     this.station_id = station.station_id;
     this.name = station.name;
+    this.address = station.address
     this.capacity = station.capacity;
     this.status = status
 }
